955d9c4f9f68d3acb6a91ee50cc1033e0759631e
NachoVazquez <nachovc1410@gmail.com>
2019-02-08 17:37:43 -0500

    Implement gameService with creation method by users names

---
+++ apps/game-of-drones/src/app/core/services/game.service.ts
@@ -0,0 +1,28 @@
+import { MatSnackBar } from '@angular/material';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BaseCrudService } from '@god/god-shared';
+import { Game } from '../models/game.model';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { publishLast, catchError } from 'rxjs/operators';
+import { retryBackoff } from 'backoff-rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class GameService extends BaseCrudService<Game, number> {
+  constructor(private httpClient: HttpClient, protected snack: MatSnackBar) {
+    super(environment.apiUrl + 'games', httpClient, snack);
+  }
+
+  createGameByPlayersNames(playersData: {
+    player1Name: string;
+    player2Name: string;
+  }): Observable<Game> {
+    return this.http
+      .post<Game>(this.url + '/createByPlayersName', playersData)
+      .pipe(
+        retryBackoff(this.retryConfig),
+        catchError(error => this.handleError(error))
+      );
+  }
+}
