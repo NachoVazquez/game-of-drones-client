4f6152d866aa648e3224a0f80820afe6a59a1bf8
NachoVazquez <nachovc1410@gmail.com>
2019-02-09 11:26:39 -0500

    Prepares Round Component to handle player turns

--- apps/game-of-drones/src/app/features/game/presentational/round/round.component.ts
+++ apps/game-of-drones/src/app/features/game/presentational/round/round.component.ts
@@ -1,5 +1,8 @@
+import { Game } from './../../../../core/models/game.model';
+import { Round } from './../../../../core/models/round.model';
 import { PlayerMoves } from './../../../../core/models/enums/player-moves.enum';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'god-round',
@@ -7,6 +10,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class RoundComponent implements OnInit {
   @Input() roundNumber = 1;
+  @Input() currentGame$: Observable<Game>;
+
+  @Output() completeRound: EventEmitter<Round> = new EventEmitter();
 
   player1Move: PlayerMoves;
   player2Move: PlayerMoves;
@@ -14,4 +20,16 @@ export class RoundComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  player1MoveSelected(move: PlayerMoves) {
+    this.player1Move = move;
+  }
+
+  player2MoveSelected(move: PlayerMoves) {
+    this.player2Move = move;
+    this.completeRound.emit({
+      player1Move: this.player1Move,
+      player2Move: this.player2Move
+    });
+  }
 }
