ef3dd84d040048a0fc78b3edf3c2b957cfcb74b9
NachoVazquez <nachovc1410@gmail.com>
2019-02-12 17:31:45 -0500

    Implement logic and feel of Home Component
    * Implements game creation

--- apps/game-of-drones/src/app/features/home/containers/home.component.ts
+++ apps/game-of-drones/src/app/features/home/containers/home.component.ts
@@ -1,17 +1,20 @@
 import { GameService } from './../../../core/services/game.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RouterSandbox } from '../../../core/facades/router-sandbox';
 
 @Component({
   selector: 'god-home',
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.scss']
 })
 export class HomeComponent implements OnInit {
   protected gameForm: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
-    protected gameService: GameService
+    protected gameService: GameService,
+    protected routerFacade: RouterSandbox
   ) {}
 
   ngOnInit() {
@@ -25,7 +28,16 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  createGame(){
-      
+  createGame() {
+    this.gameService
+      .createGameByPlayersNames({
+        player1Name: this.gameForm.get('player1Name').value,
+        player2Name: this.gameForm.get('player2Name').value
+      })
+      .subscribe(game => {
+        if (game) {
+          this.routerFacade.navigate(['game', game.id.toString()]);
+        }
+      });
   }
 }
