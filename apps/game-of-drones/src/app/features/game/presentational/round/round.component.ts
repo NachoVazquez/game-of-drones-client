4f06b99f92072f8eac3f980d57fcdc28f6178207
NachoVazquez <nachovc1410@gmail.com>
2019-02-12 17:33:53 -0500

    Implements logic and UI of round component
    * Implements round reset when a tie occurs
    * Fix round template styles

--- apps/game-of-drones/src/app/features/game/presentational/round/round.component.ts
+++ apps/game-of-drones/src/app/features/game/presentational/round/round.component.ts
@@ -1,25 +1,55 @@
 import { Game } from './../../../../core/models/game.model';
 import { Round } from './../../../../core/models/round.model';
 import { PlayerMoves } from './../../../../core/models/enums/player-moves.enum';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnDestroy
+} from '@angular/core';
+import { Observable, config } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { untilDestroyed } from 'ngx-take-until-destroy';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'god-round',
-  templateUrl: 'round.component.html'
+  templateUrl: 'round.component.html',
+  styleUrls: ['round.component.scss']
 })
-export class RoundComponent implements OnInit {
+export class RoundComponent implements OnInit, OnDestroy {
   @Input() roundNumber = 1;
   @Input() currentGame$: Observable<Game>;
+  @Input() roundReset$: Observable<number>;
 
   @Output() completeRound: EventEmitter<Round> = new EventEmitter();
 
   player1Move: PlayerMoves;
   player2Move: PlayerMoves;
 
-  constructor() {}
+  constructor(protected snack: MatSnackBar) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.roundReset$)
+      this.roundReset$
+        .pipe(untilDestroyed(this))
+        .subscribe(resetRoundNumber => {
+          if (resetRoundNumber && resetRoundNumber === this.roundNumber) {
+            this.snack.open(
+              'THERE WAS A TIE!!! PLAY UNTIL WE HAVE A WINNER!!',
+              'close',
+              { duration: 3500 }
+            );
+
+            this.player1Move = null;
+            this.player2Move = null;
+          }
+        });
+
+    if (this.currentGame$) this.currentGame$.subscribe(game => {});
+  }
 
   player1MoveSelected(move: PlayerMoves) {
     this.player1Move = move;
@@ -32,4 +62,6 @@ export class RoundComponent implements OnInit {
       player2Move: this.player2Move
     });
   }
+
+  ngOnDestroy(): void {}
 }
