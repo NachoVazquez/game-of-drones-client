1f7189c16828755bb8b5f45bbc3a9f23556a42e3
NachoVazquez <nachovc1410@gmail.com>
2019-02-12 17:59:05 -0500

    Add lazily load route for game module

--- apps/game-of-drones/src/app/app.routes.ts
+++ apps/game-of-drones/src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const appRoutes: Routes = [
     path: 'home',
     loadChildren: './features/home/home.module#HomeModule'
   },
+  {
+    path: 'game/:id',
+    loadChildren: './features/game/game.module#GameModule'
+  },
   { path: '**', redirectTo: 'home' }
 ];
