39269354d69e8bf1a6ab96d20fca2eac51100127
NachoVazquez <nachovc1410@gmail.com>
2019-02-13 03:08:17 -0500

    Implement player statistics search method on statistics service

--- apps/game-of-drones/src/app/core/services/statistics.service.ts
+++ apps/game-of-drones/src/app/core/services/statistics.service.ts
@@ -7,16 +7,26 @@ import { Observable } from 'rxjs';
 import { GlobalStatistics } from '../models/global-statistics.model';
 import { retryBackoff } from 'backoff-rxjs';
 import { catchError } from 'rxjs/operators';
+import { PlayerStatistics } from '../models/player-stats.model';
 
 @Injectable({ providedIn: 'root' })
 export class StatisticsService extends BaseCrudService<unknown, unknown> {
   constructor(private httpClient: HttpClient, private snackbar: MatSnackBar) {
-    super(environment.apiUrl + 'statistics', httpClient, snackbar);
+    super(environment.apiUrl + 'Statistics', httpClient, snackbar);
   }
 
   public getGlobalStatistics(): Observable<GlobalStatistics> {
     return this.http
       .get<GlobalStatistics>(this.url + '/GetGlobalStatistics')
+      .pipe(
+        catchError(this.handleError),
+        retryBackoff(this.retryConfig)
+      );
+  }
+
+  getPlayerStatistics(playerName: string): Observable<PlayerStatistics> {
+    return this.http
+      .get<PlayerStatistics>(this.url + '/GetPlayerStatistics/' + playerName)
       .pipe(
         catchError(this.handleError),
         retryBackoff(this.retryConfig)
